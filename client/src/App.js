d954abd6f055aa8ab28d12cea10501bd66454391
Anna Park <annapark23@gmail.com>
2019-01-15 12:27:48 -0600

    working on nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Navs from "./components/Nav"
+
+
 
 function App() {
   return (
     <div>
+      <Navs />
       hello this is a test
     </div>
   );
---
+++ client/src/components/Nav/navTabs.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./style.css";
+import { Navbar, NavItem, Nav } from "react-bootstrap";
+
+const page = "Dashboard";
+
+
+class Navs extends Component {
+
+	state ={
+		background: "#544F4F"
+	};
+
+	changeColor = () => {
+		this.setState({background:"white"});
+	};
+
+	render(){
+		return (
+			<Navbar inverse collapseOnSelect>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<a href="#brand" id ="brand">{page}</a>
+					</Navbar.Brand>
+					<Navbar.Toggle />
+				</Navbar.Header>
+				<Navbar.Collapse>
+					<Nav pullRight>
+						<NavItem eventKey={1} href="#" onClick ={this.changeColor} style={{background:this.state.background}}>
+							Dashboard
+						</NavItem>
+						<NavItem eventKey={2} href="/about" onClick ={this.changeColor} style={{background:this.state.background}}>
+							Account
+						</NavItem>
+						<NavItem eventKey={2} href="#" onClick ={this.changeColor} style={{background:this.state.background}}>
+							Sign Out
+						</NavItem>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		)
+	}
+}
+
+export default Navs;
+
