a159aed845baafc14beda2795b5e2fc5da4f87ac
greenman <caasilemroh@gmail.com>
2019-01-22 11:29:06 -0600

    Please work!

--- myOldAuthenticationStuff/models/terms.js
+++ myOldAuthenticationStuff/models/terms.js
@@ -1,53 +1,49 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('postings', {
+  return sequelize.define('terms', {
     id: {
       type: DataTypes.INTEGER(11),
       allowNull: false,
       primaryKey: true,
 			autoIncrement: true
     },
-		posting_title: {
-      type: DataTypes.STRING(255),
-      allowNull: false
-    },
-    posting_type: {
-      type: DataTypes.STRING(255),
-      allowNull: true,
-      defaultValue: ''
-    },
-    posting_desc: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    posting_tags: {
-      type: DataTypes.STRING(255),
-      allowNull: true
-    },
-    posting_complete: {
+    currently_working: {
       type: DataTypes.INTEGER(1),
       allowNull: true,
       defaultValue: '0'
     },
-    posting_completion: {
-      type: DataTypes.DATEONLY,
+    start_time: {
+      type: DataTypes.DATE,
       allowNull: true
     },
-    posting_completion_deadline: {
-      type: DataTypes.DATEONLY,
+    end_time: {
+      type: DataTypes.DATE,
       allowNull: true
     },
-    posting_owner: {
+    picture: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    paid_for: {
+      type: DataTypes.INTEGER(1),
+      allowNull: true,
+      defaultValue: '0'
+    },
+    term_summery: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    posting_employee: {
       type: DataTypes.INTEGER(11),
       allowNull: false,
       references: {
-        model: 'users',
+        model: 'posting_employees',
         key: 'id'
       }
     }
   }, {
-    tableName: 'postings',
+    tableName: 'terms',
 		timestamps: false
   });
 };
