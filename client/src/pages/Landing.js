78f62ef1d9829f5f05322f373e1af10836c5ab7e
1Cbondy1 <claytonbondy@gmail.com>
2019-01-24 10:37:53 -0600

    fix

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navs from "../components/Nav"
+import { Link } from "react-router-dom";
 
 const styling ={
     display:'none'
@@ -14,10 +15,8 @@ function Landing() {
             <p className="text-block">Justo is a robust tracking application that allows for clear real-time information regarding rates, hours, payment and invoicing, and important dates for freelancers and employers. Justo acts as the acts as the intermediary - keeping all parties on the same timeline and ensuring that fair work is rewarded with fair pay.</p>
             <div>
                 <button className="justo-button">Signup</button>
-                <button className="justo-button"><a style={{ color:"#ECECEC" }} href="/login">Login</a></button>
+                <Link to={"/login/"}><button className="justo-button">Login</button></Link>
+                
             </div>
         </div>
 );
-}
-
-export default Landing;
