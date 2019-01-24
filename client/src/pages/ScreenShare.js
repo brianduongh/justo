fca0b11e955aeb53ce8c870d73351437466a4a57
1Cbondy1 <claytonbondy@gmail.com>
2019-01-23 21:09:26 -0600

    added screenshare components

---
+++ client/src/pages/ScreenShare.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ScreenShare from "../components/ScreenShare"
+
+
+function ShowScreenShare() {
+	return (
+    	<div>
+		<ScreenShare  />
+    	</div>
+  	);
+}
+
+export default ShowScreenShare;
